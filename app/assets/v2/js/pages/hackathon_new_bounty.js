b86fcea40565f4ab817ed06e108f592619c42f14
Aditya Anand M C <aditya.anandmc@gmail.com>
2020-05-13 15:28:53 +0530

    chore: cross chain flow

--- app/assets/v2/js/pages/hackathon_new_bounty.js
+++ app/assets/v2/js/pages/hackathon_new_bounty.js
@@ -1,5 +1,6 @@
-const hackathon_slug = document.hackathon_slug
-const sponsors = document.sponsors
+const hackathon_slug = document.hackathon_slug;
+const sponsors = document.sponsors;
+
 Vue.component('v-select', VueSelect.VueSelect);
 Vue.mixin({
   methods: {
@@ -38,22 +39,23 @@ Vue.mixin({
         //   vm.errorIssueDetails = 'This issue wasn\'t bountied yet.';
         // }
       }).catch((err) => {
-        console.log(err)
+        console.log(err);
         vm.errorIssueDetails = err.responseJSON.message;
       });
 
     },
-    getTokens: function(){
+    getTokens: function() {
       let vm = this;
       const apiUrlTokens = '/api/v1/tokens/';
       const getTokensData = fetchData(apiUrlTokens, 'GET');
+
       $.when(getTokensData).then((response) => {
         vm.tokens = response;
-        vm.form.token = vm.filterByNetwork[0]
-        vm.getAmount(vm.form.token.symbol)
+        vm.form.token = vm.filterByNetwork[0];
+        vm.getAmount(vm.form.token.symbol);
 
       }).catch((err) => {
-        console.log(err)
+        console.log(err);
         // vm.errorIssueDetails = err.responseJSON.message;
       });
 
@@ -66,36 +68,40 @@ Vue.mixin({
       }
       const apiUrlAmount = `/sync/get_amount?amount=1&denomination=${token}`;
       const getAmountData = fetchData(apiUrlAmount, 'GET');
+
       $.when(getAmountData).then((response) => {
         vm.coinValue = response.usdt;
-        vm.calcValues('usd')
+        vm.calcValues('usd');
 
       }).catch((err) => {
-        console.log(err)
+        console.log(err);
         // vm.errorIssueDetails = err.responseJSON.message;
       });
     },
     calcValues: function(direction) {
       let vm = this;
+
       if (direction == 'usd') {
-        let usdValue = vm.form.amount * vm.coinValue
+        let usdValue = vm.form.amount * vm.coinValue;
+
         vm.form.amountusd = Number(usdValue.toFixed(2));
-        console.log(vm.form.amountusd)
+        console.log(vm.form.amountusd);
       } else {
         vm.form.amount = Number(vm.form.amountusd * 1 / vm.coinValue).toFixed(4);
-        console.log(vm.form.amount)
+        console.log(vm.form.amount);
       }
 
     },
     addKeyword: function(item) {
       let vm = this;
 
-      vm.form.keywords.push(item)
+      vm.form.keywords.push(item);
     },
     checkForm: function(e) {
       let vm = this;
+
       if (vm.form.bounty_categories.length < 1) {
-        vm.errors.push('Select at least one category')
+        vm.errors.push('Select at least one category');
         e.preventDefault();
       } else {
         return;
@@ -105,43 +111,175 @@ Vue.mixin({
       let vm = this;
 
       if (network === 'mainnnet') {
-        // vm.blockchainSend(form)
+        // vm.blockchainSend(metadata)
         // vm.sendData(form)
 
       } else if (network === 'custom') {
-        // vm.sendData(form)
-
+        vm.sendData();
       }
 
 
     },
+    populateMetadata: function() {
+
+      let vm = this;
+
+      const issueDetails = vm.issueDetails;
+      const form = vm.form;
+      
+      const metadata = {
+        issueTitle: issueDetails.title,
+        issueDescription: issueDetails.description,
+        issueKeywords: form.keywords ? form.keywords : '',
+        githubUsername: data.githubUsername, // XX
+        notificationEmail: data.notificationEmail, // XX
+        experienceLevel: form.experience_level,
+        projectLength: form.project_length,
+        bountyType: form.project_type,
+        fundingOrganisation: vm.orgSelected,
+        eventTag: vm.hackathon_slug,
+        repo_type: 'public',
+        tokenName: form.token && form.token.symbol,
+        bounty_categories: form.bounty_categories
+      };
+
+      return metadata;
+    },
     sendData: function() {
-      // api post method
+
+      const metadata = populateMetadata();
+      let vm = this;
+
+      const form = vm.form;
+      
+      const params = {
+        'title': metadata.issueTitle,
+        'amount': form.amount,
+        'value_in_token': form.amount * 10 ** form.token.decimals,
+        'token_name': metadata.tokenName,
+        'token_address': form.token.address,
+        'bounty_type': metadata.bountyType,
+        'project_length': metadata.projectLength,
+        'estimated_hours': metadata.estimatedHours,
+        'experience_level': metadata.experienceLevel,
+        'github_url': form.gitcoinIssueUrl,
+        'bounty_owner_email': metadata.notificationEmail, // XX
+        'bounty_owner_github_username': metadata.githubUsername, // XX
+        'expires_date': expiresDate, // XX
+        'metadata': JSON.stringify(metadata),
+        'network': 'mainnet', // XX
+        'issue_description': metadata.issueDescription,
+        'funding_organisation': metadata.fundingOrganisation,
+        'balance': form.amount * 10 ** form.token.decimals, // ETC-TODO REMOVE ?
+        'project_type': form.project_type,
+        'permission_type': form.permission_type,
+        'bounty_categories': metadata.bounty_categories,
+        'repo_type': metadata.repo_type,
+        'eventTag': metadata.eventTag,
+        'auto_approve_workers': data.auto_approve_workers ? 'True' : 'False', // XX
+        'web3_type': 'qr',
+        'bounty_owner_address': form.funderAddress
+      };
+    
+      const url = '/api/v1/bounty/create';
+    
+      $.post(url, params, function(response) {
+        if (200 <= response.status && response.status <= 204) {
+          console.log('success', response);
+          window.location.href = response.bounty_url;
+        } else if (response.status == 304) {
+          _alert('Bounty already exists for this github issue.', 'error');
+          console.error(`error: bounty creation failed with status: ${response.status} and message: ${response.message}`);
+        } else {
+          _alert('Unable to create a bounty. Please try again later', 'error');
+          console.error(`error: bounty creation failed with status: ${response.status} and message: ${response.message}`);
+        }
+      });
 
     },
     blockchainSend: function() {
+      const metadata = populateMetadata();
 
+      let vm = this;
+      
+      // let ipfsBounty = {
+      //   payload: {
+      //     title: metadata.issueTitle,
+      //     description: metadata.issueDescription,
+      //     sourceFileName: '',
+      //     sourceFileHash: '',
+      //     sourceDirectoryHash: '',
+      //     issuer: {
+      //       name: metadata.fullName,
+      //       email: metadata.notificationEmail,
+      //       githubUsername: metadata.githubUsername,
+      //       address: '' // Fill this in later
+      //     },
+      //     schemes: {
+      //       project_type: data.project_type,
+      //       permission_type: data.permission_type,
+      //       auto_approve_workers: !!data.auto_approve_workers
+      //     },
+      //     hiring: {
+      //       hiringRightNow: !!data.hiringRightNow,
+      //       jobDescription: data.jobDescription
+      //     },
+      //     funding_organisation: metadata.fundingOrganisation,
+      //     is_featured: metadata.is_featured,
+      //     repo_type: metadata.repo_type,
+      //     featuring_date: metadata.featuring_date,
+      //     privacy_preferences: privacy_preferences,
+      //     funders: [],
+      //     categories: metadata.issueKeywords.split(','),
+      //     created: (new Date().getTime() / 1000) | 0,
+      //     webReferenceURL: issueURL,
+      //     fee_amount: 0,
+      //     fee_tx_id: '0x0',
+      //     // optional fields
+      //     metadata: metadata,
+      //     tokenName: token['name'],
+      //     tokenAddress: tokenAddress,
+
+      //   },
+      //   meta: {
+      //     platform: 'gitcoin',
+      //     schemaVersion: '0.1',
+      //     schemaName: 'gitcoinBounty'
+      //   }
+      // };
+
+
+      // const bounty = web3.eth.contract(bounty_abi).at(bounty_address());
+
+      // ipfs.ipfsApi = IpfsApi(ipfsConfig);
+      // ipfs.setProvider(ipfsConfig);
+      
+      // check if token is authed
+      // check balance
+      // do_bounty -> deductBountyAmount
+    
     }
   },
   computed: {
     sortByPriority: function() {
       // console.log(elem)
       return this.tokens.sort(function(a, b) {
-        return b.priority - a.priority
-      })
+        return b.priority - a.priority;
+      });
     },
     filterByNetwork: function() {
       const vm = this;
-      if( vm.network == ''){
+
+      if (vm.network == '') {
         return vm.tokens;
       }
       return vm.sortByPriority.filter((item)=>{
 
         return item.network.toLowerCase().indexOf(vm.network.toLowerCase()) >= 0;
-      })
+      });
     }
   }
-})
+});
 
 if (document.getElementById('gc-hackathon-new-bounty')) {
   // var vueSelect = import('vue-select');
@@ -165,15 +303,15 @@ if (document.getElementById('gc-hackathon-new-bounty')) {
         coinValue: null,
         form: {
           gitcoinIssueUrl: '',
-          bounty_categories:[],
-          project_type:'',
-          permission_type:'',
+          bounty_categories: [],
+          project_type: '',
+          permission_type: '',
           keywords: [],
           amount: 0.001,
           amountusd: null,
-          token: {},
-        },
-      }
+          token: {}
+        }
+      };
     },
     mounted() {
       this.getTokens();
